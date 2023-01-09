abc749ed1b5d504dac0b99ba7cbe042c4d161e49
Vadim Voronin <v.voronin@lancktele.com>
2023-01-09 16:36:14 +0300

    split in progress

--- internal/oratool/splitter/service.go
+++ internal/oratool/splitter/service.go
@@ -2,12 +2,13 @@ package splitter
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"github.com/VadimGossip/gitPatchTool/internal/domain"
 	"github.com/VadimGossip/gitPatchTool/internal/filewalker"
 	"github.com/VadimGossip/gitPatchTool/internal/oratool/extractor"
-	"github.com/sirupsen/logrus"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -32,16 +33,17 @@ type file struct {
 	text []string
 }
 
-func (s *service) markFileLines(fileLines []string) (map[string][]int, error) {
+func (s *service) markFileLines(fileLines []string) map[int]string {
 	stopElement := ";"
-	searchTmpl := []string{"create", "any", "index", "any", "on", "any", "any", "any"}
+
 	foundLines := make([]string, 0)
-	markedMap := make(map[string][]int)
+	markedMap := make(map[int]string)
 
 	for idx, fileLine := range fileLines {
 		tmpFileLine := strings.TrimSpace(strings.ToLower(fileLine))
+		tmpFileLine = regexp.MustCompile(`\s+`).ReplaceAllString(tmpFileLine, " ")
 
-		if strings.HasPrefix(tmpFileLine, searchTmpl[0]) {
+		if strings.HasPrefix(tmpFileLine, "alter table") {
 			foundLines = []string{tmpFileLine}
 		} else {
 			foundLines = append(foundLines, tmpFileLine)
@@ -51,20 +53,103 @@ func (s *service) markFileLines(fileLines []string) (map[string][]int, error) {
 		for _, str := range foundLines {
 			if resultStr == "" {
 				resultStr = strings.TrimSpace(str)
+			} else {
+				resultStr = resultStr + " " + strings.TrimSpace(str)
 			}
-			resultStr += " " + strings.TrimSpace(str)
 		}
-
 		parts := strings.Split(resultStr, " ")
-		if len(parts) > 4 && parts[4] == "on" && (parts[2] == "index" || parts[1] == "index") && strings.HasSuffix(parts[len(parts)-1], stopElement) {
-			for i := idx; i < idx+len(foundLines); i++ {
-				markedMap[parts[3]] = append(markedMap[parts[3]], i)
+		if len(parts) > 11 &&
+			parts[0] == "alter" &&
+			parts[1] == "table" &&
+			parts[3] == "add" &&
+			parts[4] == "constraint" &&
+			parts[6] == "foreign" &&
+			parts[7] == "key" &&
+			parts[8] == "references" && strings.HasSuffix(parts[len(parts)-1], stopElement) {
+			for i := idx - (len(foundLines) - 1); i <= idx; i++ {
+				markedMap[i] = parts[3]
 			}
-			fmt.Println(resultStr)
 			foundLines = nil
 		}
 	}
-	return markedMap, nil
+	return markedMap
+}
+
+func (s *service) createDir(dirPath string) error {
+	if _, err := os.Stat(dirPath); errors.Is(err, os.ErrNotExist) {
+		if err = os.Mkdir(dirPath, os.ModePerm); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *service) createFile(filePath string, fileLines []string) error {
+	f, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	for _, fileLine := range fileLines {
+		_, err := f.WriteString(fileLine + "\n")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *service) splitTableFile(oraFile domain.OracleObject) error {
+	f, err := os.Open(oraFile.File.Path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	fileLines := make([]string, 0)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fileLines = append(fileLines, scanner.Text())
+	}
+
+	markedLines := s.markFileLines(fileLines)
+
+	filesToCreate := make(map[string][]string)
+	for idx, fileLine := range fileLines {
+		if val, ok := markedLines[idx]; ok {
+			if _, ftc := filesToCreate[val+".sql"]; !ftc {
+				schema, err := s.fileWalker.SearchStrInFile("schema", oraFile.File.Path)
+				if err == nil {
+					filesToCreate[val+".sql"] = append(filesToCreate[val], schema)
+				}
+			}
+			filesToCreate[val+".sql"] = append(filesToCreate[val+".sql"], fileLine)
+
+		} else {
+			filesToCreate[oraFile.File.Name] = append(filesToCreate[oraFile.File.Name], fileLine)
+
+		}
+	}
+	path := oraFile.File.Path
+
+	if len(filesToCreate) > 1 {
+		if err := s.createDir(path[:len(oraFile.File.Path)-len("tables"+string(os.PathSeparator)+oraFile.File.Name)] + "tables.fk"); err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	for key, val := range filesToCreate {
+		if key != oraFile.File.Name {
+			path = path[:len(oraFile.File.Path)-len("tables"+string(os.PathSeparator)+oraFile.File.Name)] + "tables.fk" + string(os.PathSeparator) + oraFile.File.Name + "." + key
+		}
+		if err := s.createFile(path, val); err != nil {
+			fmt.Println(err)
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (s *service) SplitTableFiles() error {
@@ -80,25 +165,8 @@ func (s *service) SplitTableFiles() error {
 		}
 	}
 
-	logrus.Infof("len filteredObj %d", len(filteredObj))
-
-	logrus.Info(filteredObj[1].File.Path)
-
-	f, err := os.Open(filteredObj[1].File.Path)
-	if err != nil {
+	if err := s.splitTableFile(filteredObj[1]); err != nil {
 		return err
 	}
-	defer f.Close()
-
-	fileLines := make([]string, 0)
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		//fileLine := strings.TrimSpace(strings.ToLower(scanner.Text()))
-		//fileLine = regexp.MustCompile(`\s+`).ReplaceAllString(fileLine, " ")
-		fileLines = append(fileLines, scanner.Text())
-	}
-
-	res, err := s.markFileLines(fileLines)
-	fmt.Println(res)
 	return nil
 }
