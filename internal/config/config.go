30bc9dbdb77a005f16fa318af030a872c65c5e77
Vadim Voronin <v.voronin@lancktele.com>
2023-01-18 17:05:49 +0300

    config validation

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,13 @@
 package config
 
 import (
+	"errors"
+	"fmt"
 	"github.com/VadimGossip/gitPatchTool/internal/domain"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"os"
+	"strings"
 )
 
 func parseConfigFile(configDir string) error {
@@ -27,6 +31,22 @@ func unmarshal(cfg *domain.Config) error {
 	return nil
 }
 
+func checkAndFixPaths(cfg *domain.Config) error {
+	if !strings.HasSuffix(cfg.Path.RootDir, string(os.PathSeparator)) {
+		cfg.Path.RootDir = cfg.Path.RootDir + string(os.PathSeparator)
+	}
+	if !strings.HasSuffix(cfg.Path.InstallDir, string(os.PathSeparator)) {
+		cfg.Path.RootDir = cfg.Path.InstallDir + string(os.PathSeparator)
+	}
+	if cfg.Mode == "patch" {
+		if !strings.HasPrefix(cfg.Path.InstallDir, cfg.Path.RootDir) {
+			return errors.New(fmt.Sprintf("install dir %s not in git dir %s", cfg.Path.InstallDir, cfg.Path.RootDir))
+		}
+	}
+
+	return nil
+}
+
 func Init(configDir string) (*domain.Config, error) {
 	viper.SetConfigName("config")
 	if err := parseConfigFile(configDir); err != nil {
@@ -38,6 +58,9 @@ func Init(configDir string) (*domain.Config, error) {
 		return nil, err
 	}
 
+	if err := checkAndFixPaths(&cfg); err != nil {
+		return nil, err
+	}
 	logrus.Infof("Config %v", cfg)
 
 	return &cfg, nil
